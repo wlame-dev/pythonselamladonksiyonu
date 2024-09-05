09c557d2af6370d102af5a2867db9fcb6e202f9a
kerem_dev <kerembisen12@gmail.com>
2024-09-05 11:11:37 +0300

    Create fonksiyon.py

---
+++ fonksiyon.py
@@ -0,0 +1,12 @@
+# Fonksiyon Kodu
+def selamla(selamlamaisim="İsimsiz"):
+  print("Merhaba", selamlamaisim)
+
+# Deneme
+selamla('Serdar')
+
+# Serdar Yazan Yere Farklı Farklı İsimler Yaza Bilirsiniz.
+
+# Sonuç : Merhaba (isim) Şeklinde Olur. 
+
+
